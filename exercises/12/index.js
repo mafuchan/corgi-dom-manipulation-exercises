808fd7328c76bf54900421344eadf0e073171541
Matt Crecraft <mcrecraft@gmail.com>
2022-04-04 15:47:26 -0500

    Added 12-15

---
+++ exercises/12/index.js
@@ -0,0 +1,6 @@
+const queens = document.querySelector("div.queens")
+const li = document.createElement("li")
+li.textContent = "Kiki"
+const ul = document.createElement("ul")
+ul.append(li)
+queens.append(ul)
---
+++ exercises/13/index.js
@@ -0,0 +1,4 @@
+const body = document.querySelector("body")
+const h2 = document.createElement("h2")
+h2.textContent = "Iago"
+body.append(h2)
