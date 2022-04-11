fd6381c4901d3f5cc22bbdaf9085ae6ffcf47c09
Matt Crecraft <mcrecraft@gmail.com>
2022-04-11 15:15:39 -0500

    Updated classList again

--- exercises/4/index.js
+++ exercises/4/index.js
@@ -1,2 +1,3 @@
 const lis = document.querySelectorAll("li")
-lis.forEach(li => li.classList.add = "corgi")
+lis.forEach(li => li.classList.remove("dog"))
+lis.forEach(li => li.classList.add("corgi"))
